7a39d6fd3a54b915bf80915aaddd9b3d77421225
shubham-jr <shubhampandeybhu21@gmail.com>
2023-12-19 11:47:55 +0530

    ✨ New feature added smtp config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -4,3 +4,5 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
 export const { DB_PASSWORD } = process.env;
+export const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } = process.env;
+export const { SMTP_HOST, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD } = process.env;
